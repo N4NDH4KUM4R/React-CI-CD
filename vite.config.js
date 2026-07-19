feca754b3783cc1639c684110a068a07a7aa3905
Nandha Kumar <nchinnasami@siddhaai.com>
2026-07-19 15:38:10 +0530

    Update GitHub Actions workflow

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/React-CI-CD/",
+  server: {
+    port: 3007,
+    host: true,
+  },
   plugins: [react()],
 })
